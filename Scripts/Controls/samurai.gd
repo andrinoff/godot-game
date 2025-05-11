a7a13f056fac4e4668e69d830313b6894e23d5dc
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-05-12 03:46:16 +0400

    added basic physics

--- Scripts/Controls/samurai.gd
+++ Scripts/Controls/samurai.gd
@@ -9,27 +9,15 @@ func _init() -> void:
 	$".".play("idle")
 func _process(_delta) -> void:
 	if Input.is_action_pressed("ui_left"):
-		if position.x >= 0:
-			$".".scale.x = -2
-			position.x -= 1
+		$".".scale.x = -2
 		$".".play("walk")
 	if Input.is_action_just_released("ui_left"):
 		$".".play("idle")
 	if Input.is_action_pressed("ui_right"):
-		if position.x <= 1920:
-			$".".scale.x = 2
-			position.x += 1
+		$".".scale.x = 2
 		$".".play("walk")
 	if Input.is_action_just_released("ui_right"):
 		$".".play("idle")
-	if Input.is_action_just_pressed("ui_up"):
-
-		if position.y>=0 and position.x <= 1920:
-			$".".play("jump")
-			var tween = create_tween()
-			tween.tween_property(self, "position", position - Vector2(-50, 100), 1)
-			await self.animation_finished
-			$".".play("idle")
-		else:
-			pass
+	
 # FIXME: You can jump and move at the same time
+# FIXME: Player turns into a pickle
--- Scripts/start_screen/button.gd
+++ Scripts/start_screen/button.gd
@@ -2,4 +2,4 @@ extends Button
 
 func _on_pressed() -> void:
 	print("Button pressed")
-	get_tree().change_scene_to_file("res://Scenes/control_learning.tscn")
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
